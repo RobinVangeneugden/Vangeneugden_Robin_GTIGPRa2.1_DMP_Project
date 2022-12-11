56931c58c22ac78293e0b57257703f255ab5b172
Robin Vangeneugden <r0318322@student.thomasmore.be>
2022-12-11 21:36:54 +0100

    First Commit Add Design MainWindow + Add Pages

---
+++ Vangeneugden_Robin_DMP_Project_WPF/DeleteBandlid.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vangeneugden_Robin_DMP_Project_WPF
+{
+    /// <summary>
+    /// Interaction logic for DeleteBandlid.xaml
+    /// </summary>
+    public partial class DeleteBandlid : Page
+    {
+        public DeleteBandlid()
+        {
+            InitializeComponent();
+        }
+    }
+}
